043feb55312b778a57b90d4af5e1fb384e67015f
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-07-29 02:18:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,12 @@
 require "pry"
 
 def speak_to_grandma(phrase)
-  binding.pryif phrase == "I LOVE YOU GRANDMA!"
+  binding.pry
+  if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-    
+    elsif phrase == phrase.upcase
+      "NO, NOT SINCE 1938!"
+    else
+      "HUH?! SPEAK UP, SONNY!"
   end
 end
